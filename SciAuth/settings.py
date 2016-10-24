1ce5c7b2e25a7207fe73daff8c68228536e11df0
mtmcduffie <mcduffie.michael@gmail.com>
2016-10-24 13:28:11 -0400

    Update settings.py

--- SciAuth/settings.py
+++ SciAuth/settings.py
@@ -147,7 +147,7 @@ STATIC_URL = '/static/'
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = (
-    normpath(join(SITE_ROOT, 'static')),
+    normpath(join(SITE_ROOT, 'SciAuth', 'assets')),
 )
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders

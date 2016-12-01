e4fd511ea1cb7a79fb043c7d4d6c8f9c895cf9f3
mtmcduffie <mcduffie.michael@gmail.com>
2016-11-30 21:51:37 -0500

    Debug info.

--- SciAuth/settings.py
+++ SciAuth/settings.py
@@ -150,16 +150,13 @@ STATIC_ROOT = normpath(join(SITE_ROOT, 'SciAuth', 'static'))
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = '/static/'
 
-# See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
-STATICFILES_DIRS = (
-    normpath(join(SITE_ROOT, 'SciAuth', 'static')),
-)
-
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
+
+print(STATIC_ROOT)
 
 try:
     from .local_settings import *

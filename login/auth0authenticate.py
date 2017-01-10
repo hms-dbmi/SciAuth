0c12580cc02c481705f767b9488c3f820c91a60a
mtmcduffie <mcduffie.michael@gmail.com>
2017-01-10 15:57:34 -0500

    Adding some comments, organizing.

--- login/auth0authenticate.py
+++ login/auth0authenticate.py
@@ -1,17 +1,22 @@
 from django.contrib.auth.models import User
-import logging
-logger = logging.getLogger(__name__)
 
 
 class Auth0Authentication(object):
 
     def authenticate(self, **token_dictionary):
-        logger.debug("Attempting to Authenticate User - " + token_dictionary["email"])
+        """
+        Custom authenticate method for logging a user into the SciAuth App via their e-mail address.
+
+        :param token_dictionary:
+        :return:
+        """
+
+        print("Attempting to Authenticate User - " + token_dictionary["email"])
 
         try:
             user = User.objects.get(username=token_dictionary["email"])
         except User.DoesNotExist:
-            logger.debug("User not found, creating.")
+            print("User not found, creating.")
 
             user = User(username=token_dictionary["email"], email=token_dictionary["email"])
             user.is_staff = True
